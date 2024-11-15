1ee0d48e60bef962882b91c2ff47c313cd7b3d39
GeorgiiMalishev <malishev292@gmail.com>
2024-11-15 16:43:31 +0500

    изменение логики вычисления, завершение бэкенд части вычислений

--- calculatorApp/formula.py
+++ calculatorApp/formula.py
@@ -1,48 +1,29 @@
-from typing import Dict, Any
+from typing import Dict, Any, Callable, List
 
 
 class Formula:
-    def __init__(self, value1_key: str, operation: str, value2_key: str = None, constant: float = None):
+    def __init__(self, expression: Callable[..., float], keys: List[str]):
         """
-        Универсальный конструктор для двух случаев:
-        1. Операция между двумя переменными (value1_key и value2_key).
-        2. Операция между переменной (value1_key) и константой (constant).
-        
-        :param value1_key: ключ первого значения из словаря данных
-        :param operation: знак операции (+, -, *, /)
-        :param value2_key: ключ второго значения (если операция с двумя переменными)
-        :param constant: константа, если операция с переменной и константой
+        Конструктор, принимающий лямбда-выражение для выполнения произвольной операции с произвольным числом переменных.
+
+        :param expression: Лямбда-выражение, определяющее операцию.
+        :param keys: Список ключей для значений, которые будут переданы в лямбда-выражение.
         """
-        self.value1_key = value1_key
-        self.value2_key = value2_key
-        self.constant = constant
-        self.operation = operation
+        self.expression = expression
+        self.keys = keys
 
     def evaluate(self, data: Dict[str, Any], result_key: str) -> None:
         """
-        Выполняет вычисление на основе значений из data и записывает результат по ключу result_key.
-        Если значение не нулевое, используется оно, иначе используется константа.
+        Выполняет вычисление на основе лямбда-выражения и значений из data и сохраняет результат по ключу result_key.
+
+        :param data: Словарь с данными для вычислений.
+        :param result_key: Ключ для сохранения результата в словаре data.
         """
-        value1 = data.get(self.value1_key, 0)
+        # Извлекаем значения по ключам, если ключ не найден, подставляем 0
+        values = [data.get(key) for key in self.keys]
 
-        # Если второй ключ задан, это операция между двумя значениями
-        if self.value2_key:
-            value2 = data.get(self.value2_key, 0)
-        else:
-            # Если второй ключ не задан, используем константу
-            value2 = self.constant
-
-        # Выполняем операцию
-        if self.operation == '+':
-            result = value1 + value2
-        elif self.operation == '-':
-            result = value1 - value2
-        elif self.operation == '*':
-            result = value1 * value2
-        elif self.operation == '/':
-            result = value1 / value2
-        else:
-            raise ValueError(f"Unsupported operation: {self.operation}")
+        # Выполняем лямбда-выражение с полученными значениями
+        result = self.expression(*values)
 
         # Сохраняем результат в словарь
         data[result_key] = result
